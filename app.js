bc91b7a93348d88a102a3772f501bc40be198b59
sunny patel <sunnykpatel.skp@gmail.com>
2023-08-08 16:01:25 -0400

    add lorem ipsum logic one

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const loremText = [
   'Airedale hard cheese mozzarella. Pecorino melted cheese port-salut emmental babybel cheese and wine melted cheese manchego. Everyone loves blue castello everyone loves fromage cheese slices airedale cheddar cream cheese. Bavarian bergkase who moved my cheese halloumi port-salut gouda jarlsberg ricotta rubber cheese. Stinking bishop smelly cheese brie.',
 
   'Salvia glossier subway tile, leggings mustache YOLO semiotics chia. Pitchfork tbh af blog church-key meggings vaporware PBR&B master cleanse post-ironic man bun pabst mustache letterpress synth. Snackwave raw denim godard, 3 wolf moon shaman offal kitsch unicorn live-edge selvage schlitz fashion axe vaporware drinking vinegar prism. Shabby chic tacos artisan, chambray chicharrones cardigan leggings typewriter af pop-up williamsburg meditation PBR&B viral. You probably havent heard of them DIY jean shorts subway tile fashion axe bushwick kitsch tumeric cloud bread vaporware freegan franzen pork belly chicharrones banh mi.',
+
   'Man braid celiac synth freegan readymade, pitchfork fam salvia waistcoat lomo bitters gentrify four loko. Pitchfork semiotics post-ironic vegan. Tofu meditation microdosing hashtag semiotics venmo. Flexitarian vape tilde taiyaki. Prism poutine farm-to-table, messenger bag vegan taxidermy tattooed sartorial squid jean shorts fixie selvage trust fund vape.',
 
   'Rutters Plate Fleet boom chandler Brethren of the Coast handsomely lookout marooned brigantine knave. Buccaneer gangway jack rum loot spyglass line Jack Tar fore gaff. Gaff topmast scuttle ballast swab draught measured fer yer chains dance the hempen jig Chain Shot yardarm.',
@@ -18,11 +19,13 @@ const loremText = [
 
 const form = document.querySelector('form');
 const input = document.querySelector('#value');
+const container = document.querySelector('.container');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const value = input.value;
+  const randomNumber = Math.floor(Math.random() * loremText.length);
   if (!value || value > 9 || value < 0) {
-    console.log('hello');
+    container.innerHTML = `<p>${loremText[randomNumber]}</p>`;
   }
 });
